a862ce578776112e8a79a889b15ce4d6a230808c
Chad <chadndonahue@gmail.com>
2022-02-05 17:11:31 -0500

    added inquirer and added lib folder/finished it

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+const employees = [];
