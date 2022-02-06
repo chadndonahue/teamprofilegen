a97b7dccf4df1fc93eca3eb7d43e2a5e7a686583
Chad <chadndonahue@gmail.com>
2022-02-05 20:11:38 -0500

    added employee test and changed dependencies so tests work

---
+++ __tests__/employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require("../lib/employee");
+
+describe("Employee posts info", () => {
+  it("name", () => {
+    const i = new Employee("Miguel", "500", "Migueliscute@gmail.com");
+    expect(i.name).toBe("Miguel");
+  });
+  it("id", () => {
+    const i = new Employee("Miguel", "500", "Migueliscute@gmail.com");
+    expect(i.id).toBe("500");
+  });
+  it("email", () => {
+    const i = new Employee("Miguel", "500", "Migueliscute@gmail.com");
+    expect(i.email).toBe("Migueliscute@gmail.com");
+  });
+});
+
+describe("Employee is getting information", () => {
+  it("getName", () => {
+    const i = new Employee("Mario", "501", "Marioiscute@gmail.com");
+    expect(i.getName()).toBe("Mario");
+  });
+  it("getId", () => {
+    const i = new Employee("Mario", "501", "Marioiscute@gmail.com");
+    expect(i.getId()).toBe("501");
+  });
+  it("getEmail", () => {
+    const i = new Employee("Mario", "501", "Marioiscute@gmail.com");
+    expect(i.getEmail()).toBe("Marioiscute@gmail.com");
+  });
+  it("getRole", () => {
+    const i = new Employee("Mario", "501", "Marioiscute@gmail.com");
+    expect(i.getRole()).toBe("Employee");
+  });
+});
